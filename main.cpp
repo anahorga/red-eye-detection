89b1ef67c3c20094d99a4a77770230657b0d4874
anahorga <ana.horga9@gmail.com>
2025-05-06 11:51:39 +0300

    Last details

--- main.cpp
+++ main.cpp
@@ -10,9 +10,9 @@ int main() {
 
 
     //Mat source = imread("C:\\Users\\anaho\\OneDrive - Technical University of Cluj-Napoca\\Documents\\PI\\Proiect\\RedEyeDetection\\images\\redeyes1.jpg",
-                  //IMREAD_COLOR);
+              //   IMREAD_COLOR);
 
-    Mat source = imread("C:\\Users\\anaho\\OneDrive - Technical University of Cluj-Napoca\\Documents\\PI\\Proiect\\RedEyeDetection\\images\\img4.jpg",
+    Mat source = imread("C:\\Users\\anaho\\OneDrive - Technical University of Cluj-Napoca\\Documents\\PI\\Proiect\\RedEyeDetection\\images\\img5.jpg",
                     IMREAD_COLOR);
 
     imshow("Original Image", source);
